d3a277d98865012e7cc3adf742c096fb1eaeaaaa
DuooIncc <37597972+DuooIncc@users.noreply.github.com>
2019-04-08 19:35:01 +0100

    Update threesixtyTask.php

--- src/Swourire/AdminTroll/Task/threesixtyTask.php
+++ src/Swourire/AdminTroll/Task/threesixtyTask.php
@@ -1,31 +1,34 @@
 <?php
+
 namespace Swourire\AdminTroll\Task;
 
 use Swourire\AdminTroll\Main;
 use pocketmine\scheduler\Task;
 use pocketmine\Player;
 
-
-class threesixtyTask extends Task{
+class threesixtyTask extends Task
+{
     public $yaw = 0;
 
     private $plugin;
 
     private $player;
 
-    public function __construct(Main $plugin, Player $player){
+    public function __construct(Main $plugin, Player $player)
+    {
         $this->plugin = $plugin;
         $this->player = $player;
     }
 
-    public function onRun(int $tick){
+    public function onRun(int $tick)
+    {
         $yaw = $this->player->getYaw();
 
-        while($this->yaw < 360){
+        while ($this->yaw < 360) {
             $this->player->teleport($this->player->getPosition(), $this->yaw++);
         }
 
-        if($this->yaw === 360){
+        if ($this->yaw === 360) {
             $this->yaw = 0;
             $this->getHandler()->cancel();
         }
