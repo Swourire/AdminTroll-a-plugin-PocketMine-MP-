617ebbcdc0e8f2f049a10bea83a7a4b77402e6d2
AIPTU3110 <67915267+AIPTU3110@users.noreply.github.com>
2021-01-05 01:03:01 +0700

    Lightning Again

--- src/Swourire/AdminTroll/Troll/Trolls/RealLightning.php
+++ src/Swourire/AdminTroll/Troll/Trolls/RealLightning.php
@@ -6,6 +6,7 @@ namespace Swourire\AdminTroll\Troll\Trolls;
 
 use pocketmine\entity\Entity;
 use pocketmine\network\mcpe\protocol\AddActorPacket;
+use pocketmine\math\Vector3;
 use Swourire\AdminTroll\Troll\TrollBase;
 
 class RealLightning extends TrollBase
@@ -23,13 +24,13 @@ class RealLightning extends TrollBase
         $level = $player->getLevel();
 
         $lightning = new AddActorPacket();
-        $lightning->type = Entity::LIGHTNING_BOLT;
-        $lightning->entityRuntimeId = Entity::$entityCount++;
-        $lightning->metadata = [];
-        $lightning->position = $player->asPosition();
-        $lightning->yaw = 0;
-        $lightning->pitch = 0;
-
+	$lightning->type = "minecraft:lightning_bolt";
+	$lightning->entityRuntimeId = Entity::$entityCount++;
+	$lightning->metadata = [];
+	$lightning->motion = null;
+	$lightning->yaw = $player->getYaw();
+	$lightning->pitch = $player->getPitch();
+	$lightning->position = new Vector3($player->getX(), $player->getY(), $player->getZ());
         $player->getServer()->broadcastPacket($level->getPlayers(), $lightning);
     }
 
